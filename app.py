d0046da18686794e8ebac30e30b78e0697af8c78
oheaghra <oheaghra@yahoo.com>
2026-06-13 08:52:34 -0400

    Fix duplicate entries in mapping dictionary

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def clean_housing(text):
 def clean_webex(staff_name, staff_email, service_name):
     text = (str(staff_name) + " " + str(staff_email) + " " + str(service_name)).upper()
     mapping = {"ALPHA": "A", "APOD": "A", "BRAVO": "B", "BPOD": "B", "CHARLIE": "C", 
-               "CPOD": "C", "WEBEXC": "C", "EDWINAPOD": "E", "EPOD": "E", "EDWINA": "E", "FRANKIE": "F", 
+               "CPOD": "C", "WEBEXC": "C", "EDWINAPOD": "E", "EPODWEBEX": "E", "EDWINA": "E", "FRANKIE": "F", 
                "FPOD": "F", "G POD": "G", "WEBEXG": "G", "INDIA": "I", "IPOD": "I", 
                "WEBEXI": "I", "CENTRAL": "CENTRAL", "H POD MALE": "F", "H POD FEMALE": "E"}
     for key, val in mapping.items():
